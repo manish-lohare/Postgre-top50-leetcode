810741882f5307890a02321d0b189904114d8556
Manish Lohare <158021993+manish-lohare@users.noreply.github.com>
2025-02-06 21:20:38 +0530

    Create solution.sql

---
+++ 1907. Count Salary Categories/solution.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select 'Low Salary' as category, count(*) as accounts_count from Accounts where income <20000
+union
+select 'Average Salary' as category, count(*) as accounts_count from Accounts where income between 20000 and 50000
+union
+select 'High Salary' as category, count(*) as accounts_count from Accounts where income >50000
+order by accounts_count desc
